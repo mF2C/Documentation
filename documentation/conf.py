57b325f294fbb484df8716d3661f23ff67a257ff
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-09-30 14:23:20 +0200

    update

--- documentation/conf.py
+++ documentation/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['m2r']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -338,10 +338,6 @@ texinfo_documents = [
 #
 # texinfo_no_detailmenu = False
 
-extensions = [
-    ...,
-    'm2r',
-]
 
 # source_suffix = '.rst'
 source_suffix = ['.rst', '.md']
