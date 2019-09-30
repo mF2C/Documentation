131f3f44133629ffc195c318b8516c759be5bc8d
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-09-30 14:19:41 +0200

    update

--- documentation/conf.py
+++ documentation/conf.py
@@ -337,3 +337,11 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+extensions = [
+    ...,
+    'm2r',
+]
+
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
